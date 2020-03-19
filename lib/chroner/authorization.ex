6479c3c01462f5ec0d4a940feecbd7ed53ade3ec
pozhega <mpozega94@gmail.com>
2020-03-19 19:06:49 +0100

    Remove config module

--- lib/chroner/authorization.ex
+++ lib/chroner/authorization.ex
@@ -2,25 +2,30 @@ defmodule Chroner.Authorization do
   use OAuth2.Strategy
 
   alias OAuth2.{AccessToken, Client}
-  alias Chroner.Config
+
+  @type config :: %{
+          optional(:client_id) => String.t(),
+          optional(:client_secret) => String.t(),
+          optional(:redirect_uri) => String.t(),
+          optional(:access_token) => String.t()
+        }
 
   # --------------------------------------------------------------------
   # Client
   # --------------------------------------------------------------------
 
-  def client(opts \\ []) do
-    token = if opts[:access_token], do: AccessToken.new(opts[:access_token])
-
+  @spec client(config) :: Client.t()
+  def client(config \\ %{}) do
     Client.new(
       authorize_url: "https://drchrono.com/o/authorize",
-      client_id: opts[:config][:client_id] || Config.client_id(),
-      client_secret: opts[:config][:client_secret] || Config.client_secret(),
-      redirect_uri: opts[:config][:redirect_uri] || Config.redirect_uri(),
+      client_id: Map.get(config, :client_id),
+      client_secret: Map.get(config, :client_secret),
+      redirect_uri: Map.get(config, :redirect_uri),
       request_opts: [follow_redirect: true],
       serializers: %{"application/json" => Poison},
       site: "https://app.drchrono.com/api",
       strategy: __MODULE__,
-      token: token,
+      token: config |> Map.get(:access_token) |> AccessToken.new(),
       token_url: "https://drchrono.com/o/token/"
     )
   end
@@ -29,10 +34,10 @@ defmodule Chroner.Authorization do
   # API
   # --------------------------------------------------------------------
 
-  def authorize_url!(scope), do: OAuth2.Client.authorize_url!(client(), scope: scope)
+  def authorize_url!(client, scope), do: Client.authorize_url!(client, scope: scope)
 
-  def get_token!(params \\ [], headers \\ [], opts \\ []),
-    do: OAuth2.Client.get_token!(client(), params, headers, opts)
+  def get_token!(client, params \\ [], headers \\ [], opts \\ []),
+    do: Client.get_token!(client, params, headers, opts)
 
   # --------------------------------------------------------------------
   # Strategy callbacks
