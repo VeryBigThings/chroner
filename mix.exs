f365bbaf6eb6f8475f92cd978721ebbbf50ff23c
pozhega <mpozega94@gmail.com>
2020-03-23 18:25:54 +0100

    Bump elixir version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Chroner.MixProject do
     [
       app: :chroner,
       version: "0.1.0",
-      elixir: "~> 1.8",
+      elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       dialyzer: dialyzer(),
