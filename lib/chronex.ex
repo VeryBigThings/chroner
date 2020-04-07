9ec072fabc12ca7da990699b972f123620c6c264
pozhega <mpozega94@gmail.com>
2020-04-07 19:11:14 +0200

    Update delegates in chronex.ex

--- lib/chronex.ex
+++ lib/chronex.ex
@@ -20,53 +20,79 @@ defmodule Chronex do
   # Administrative
   # --------------------------------------------------------------------
 
-  def doctors_list(client), do: apply(api_module(Authorization), :doctors_list, [client])
-  def doctors_read(client, id), do: apply(api_module(Authorization), :doctors_read, [client, id])
-  def users_current(client), do: apply(api_module(Authorization), :users_current, [client])
-  def users_list(client), do: apply(api_module(Authorization), :users_list, [client])
-  def users_read(client, id), do: apply(api_module(Authorization), :users_read, [client, id])
-  def user_groups_list(client), do: apply(api_module(Authorization), :user_groups_list, [client])
+  def doctors_list(client), do: apply(api_module(Administrative), :doctors_list, [client])
+  def doctors_read(client, id), do: apply(api_module(Administrative), :doctors_read, [client, id])
+  def users_current(client), do: apply(api_module(Administrative), :users_current, [client])
+  def users_list(client), do: apply(api_module(Administrative), :users_list, [client])
+  def users_read(client, id), do: apply(api_module(Administrative), :users_read, [client, id])
+  def user_groups_list(client), do: apply(api_module(Administrative), :user_groups_list, [client])
 
   def user_groups_read(client, id),
-    do: apply(api_module(Authorization), :user_groups_read, [client, id])
+    do: apply(api_module(Administrative), :user_groups_read, [client, id])
 
   # --------------------------------------------------------------------
   # Clinical
   # --------------------------------------------------------------------
 
-  def appointment_profiles_list(client, filters),
-    do: apply(api_module(Authorization), :appointment_profiles_list, [client, filters])
+  def appointments_list(client, filters \\ %{}),
+    do: apply(api_module(Clinical), :appointments_list, [client, filters])
+
+  def appointments_create(client, params),
+    do: apply(api_module(Clinical), :appointments_create, [client, params])
+
+  def appointments_delete(client, id),
+    do: apply(api_module(Clinical), :appointments_delete, [client, id])
+
+  def appointments_partial_update(client, id, params),
+    do: apply(api_module(Clinical), :appointments_partial_update, [client, id, params])
+
+  def appointments_read(client, id),
+    do: apply(api_module(Clinical), :appointments_read, [client, id])
+
+  def appointment_profiles_list(client, filters \\ %{}),
+    do: apply(api_module(Clinical), :appointment_profiles_list, [client, filters])
 
   def appointment_profiles_create(client, params),
-    do: apply(api_module(Authorization), :appointment_profiles_create, [client, params])
+    do: apply(api_module(Clinical), :appointment_profiles_create, [client, params])
 
   def appointment_profiles_delete(client, id),
-    do: apply(api_module(Authorization), :appointment_profiles_delete, [client, id])
+    do: apply(api_module(Clinical), :appointment_profiles_delete, [client, id])
 
   def appointment_profiles_partial_update(client, id, params),
-    do:
-      apply(api_module(Authorization), :appointment_profiles_partial_update, [client, id, params])
+    do: apply(api_module(Clinical), :appointment_profiles_partial_update, [client, id, params])
 
   def appointment_profiles_read(client, id),
-    do: apply(api_module(Authorization), :appointment_profiles_read, [client, id])
+    do: apply(api_module(Clinical), :appointment_profiles_read, [client, id])
 
   def appointment_profiles_update(client, id, params),
-    do: apply(api_module(Authorization), :appointment_profiles_update, [client, id, params])
+    do: apply(api_module(Clinical), :appointment_profiles_update, [client, id, params])
 
-  def patients_list(client, filters),
-    do: apply(api_module(Authorization), :patients_list, [client, filters])
+  def offices_add_exam_room(client, id, params),
+    do: apply(api_module(Clinical), :offices_add_exam_room, [client, id, params])
+
+  def offices_list(client, filters \\ %{}),
+    do: apply(api_module(Clinical), :offices_list, [client, filters])
+
+  def offices_read(client, id),
+    do: apply(api_module(Clinical), :offices_read, [client, id])
+
+  def offices_update(client, id, params),
+    do: apply(api_module(Clinical), :offices_update, [client, id, params])
+
+  def patients_list(client, filters \\ %{}),
+    do: apply(api_module(Clinical), :patients_list, [client, filters])
 
   def patients_create(client, params),
-    do: apply(api_module(Authorization), :patients_create, [client, params])
+    do: apply(api_module(Clinical), :patients_create, [client, params])
 
   def patients_delete(client, id),
-    do: apply(api_module(Authorization), :patients_delete, [client, id])
+    do: apply(api_module(Clinical), :patients_delete, [client, id])
 
   def patients_read(client, id),
-    do: apply(api_module(Authorization), :patients_read, [client, id])
+    do: apply(api_module(Clinical), :patients_read, [client, id])
 
   def patients_update(client, id, params),
-    do: apply(api_module(Authorization), :patients_update, [client, id, params])
+    do: apply(api_module(Clinical), :patients_update, [client, id, params])
 
   # --------------------------------------------------------------------
   # Billing
